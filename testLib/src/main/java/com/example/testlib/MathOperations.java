04a10faf75e8755b45ce600288ac1d4a2a2d7cd2
Sridhar <sridhar.k@thundersoft.com>
2023-02-15 16:15:30 +0530

    Add

---
+++ testLib/src/main/java/com/example/testlib/MathOperations.java
@@ -0,0 +1,18 @@
+package com.example.testlib;
+
+import android.content.Context;
+
+public class MathOperations {
+    double addition(Context c,double num1, double num2){
+        return num1+num2;
+    }
+    double subtraction(Context c,double num1, double num2){
+        return num1-num2;
+    }
+    double multiplication(Context c,double num1, double num2){
+        return num1*num2;
+    }
+    double division(Context c,double num1, double num2){
+        return num1/num2;
+    }
+}
