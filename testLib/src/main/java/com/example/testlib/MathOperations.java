f4fb0167a2559791aae7541a908d197b9ea3f654
Sridhar <sridhar.k@thundersoft.com>
2023-02-15 16:52:34 +0530

    Changes

--- testLib/src/main/java/com/example/testlib/MathOperations.java
+++ testLib/src/main/java/com/example/testlib/MathOperations.java
@@ -3,16 +3,16 @@ package com.example.testlib;
 import android.content.Context;
 
 public class MathOperations {
-    double addition(Context c,double num1, double num2){
+    static double addition(Context c,double num1, double num2){
         return num1+num2;
     }
-    double subtraction(Context c,double num1, double num2){
+    static double subtraction(Context c,double num1, double num2){
         return num1-num2;
     }
-    double multiplication(Context c,double num1, double num2){
+    static double multiplication(Context c,double num1, double num2){
         return num1*num2;
     }
-    double division(Context c,double num1, double num2){
+    static double division(Context c,double num1, double num2){
         return num1/num2;
     }
 }
